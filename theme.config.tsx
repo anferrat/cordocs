12bfabcc97dbdf731fa7aca650d86fa2293bb67f
Andrei Lomtev <andrei.lomtev@gmail.com>
2024-11-22 00:17:53 -0800

    Changed config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>Corpad Documentation</span>,
+  logo: <span>Corpad documentation</span>,
   project: {
     link: "https://github.com/anferrat/cordocs",
   },
   docsRepositoryBase: "https://github.com/anferrat/cordocs",
   footer: {
     text: "Corpad, 2024. Created by Andrei Lomtev",
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Nextra'
+      }
+    }
   },
 };
 
